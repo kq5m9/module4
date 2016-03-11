c7cd8272708349270c3c1a2cbe99c45fcd472ae6
kq5m9 <lazer321+github2@gmail.com>
2016-03-11 13:06:35 -0500

    remove logs

--- js/script.js
+++ js/script.js
@@ -92,7 +92,7 @@ $ajaxUtils.sendGetRequest(
 // Builds HTML for the home page based on categories array
 // returned from the server.
 function buildAndShowHomeHTML (categories) {
-  console.log("buildAndShowHomeHTML Called:", categories);
+  // console.log("buildAndShowHomeHTML Called:", categories);
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
